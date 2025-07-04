ec0deca26acf5fb3cbf6f6b3782735081f0283f2
PeriHidayatDev <perihidayat2000@gmail.com>
2025-07-04 17:47:05 +0700

    done task tugas 2

---
+++ Tugas 2/type.dart
@@ -0,0 +1,8 @@
+void main() {
+  String variable1 = "MAQDIS",
+         variable2 = "Academy",
+         variable3 = "is",
+         variable4 = "awesome";
+
+  print("$variable1 $variable2 $variable3 $variable4");
+}
