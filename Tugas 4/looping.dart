1b495aa3fe229b693bd25caffbe22fb11665cec8
PeriHidayatDev <perihidayat2000@gmail.com>
2025-07-04 18:57:02 +0700

    done task tugas 4

---
+++ Tugas 4/looping.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('LOOPING PERTAMA');
+  for (int i = 2; i <= 20; i += 2) {
+    print('$i - I love coding');
+  }
+
+  print('LOOPING KEDUA');
+  for (int i = 20; i >= 2; i -= 2) {
+    print('$i - I will become a mobile developer');
+  }
+}
